38b6ff4d66d0c1c5a3dc3c40a6b62aa5789d338c
Jose David <jose7312000@gmail.com>
2022-10-31 15:24:31 -0500

    Configurando servidores, y abriendo archivos estaticos

---
+++ src/app.js
@@ -0,0 +1,7 @@
+//Servidor
+import express from 'express';
+import path from 'path';
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import app from './app';
+//Importamos socket.io y creamos un alias cuyo nombre es WebsocketServer
+import {Server as WebsocketServer} from 'socket.io';
+import http from 'http';
+
+//Creamos un servidor http el cual servirá para ejecutar Websocket
+const server = http.createServer(app);
+const httpServer = server.listen(3000);
+//Creamos un servidor de Websocket, la conexion que vamos a tener con el cliente
+const io = new WebsocketServer(httpServer);
 console.log("Hola mundo")

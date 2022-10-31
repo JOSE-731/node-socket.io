6be6ae55c4f9932f9b73d5e10172b0ee5568759e
Jose David <jose7312000@gmail.com>
2022-10-31 16:37:03 -0500

    Comunicando el servidor con el cliente

--- src/public/main.js
+++ src/public/main.js
@@ -1,3 +1,6 @@
 //Este objeto sirve para especificarle al servidor que metodo usaremos para disparar eventos
 //Es la forma en la que se comunica el cliente y el servidor
 const socket = io()
+socket.on('loadnotes', (data) => {
+    console.log(data);
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,7 +1,20 @@
+//Importamos el modelo
+import Note from './models/Note'
+
 //Definimos los eventos de sockets
 export default(io) =>{
     //Ejecutar conexion con el servidor
     io.on('connection', () =>{
-        console.log("Hola");
+
+        //Hacemos una consulta que se ejecuta cada que visitan la pagina
+        const emitNotes = async () =>{
+            const notes = await Note.find()
+            console.log(notes)
+
+            //Este metodo sirve para enviar data al cliente
+            io.emit('loadnotes', notes);
+        }
+
+        emitNotes();
     })
 }
